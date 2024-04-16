eade55c10a878808e6f59e959475bff5146d57e7
A.A.Popova <bananabomm2003@mail.ru>
2024-04-16 13:18:32 +0700

    Добавлено изменение для дочернего процесса

--- context1.c
+++ context1.c
@@ -4,12 +4,32 @@
 #include <stdio.h>
 #include <unistd.h>
 // главня фкнкция программы
-int main(int argc, char *argv[], char *envp[]) {
-	int result;
-	//Запуск программы cat с аргументом otch1.txt
-	result = execle("/bin/cat", "/bin/cat", "otchet_16.04.2024.txt", 0, envp);
-	//printf() выполняется тольлко в случае ошибки вызова  execle()
-	printf("ошибка при выполнении системного вызова execle()\n");
-	exit(-1);
+int main(int argc, char *argv[], char *envp[])  {
+	// обьявление переменных
+	int result1;
+	pid_t procid, parentid; // ид.процессов
+	pid_t result; // рез. системного вызова fork()
+	int a = 0; // переменная для проверки работы процесса-родителя и процесса-ребенка
+	result = fork(); 
+	if (result < 0) {
+		printf("ошибка при выполнении fork()\n");
+		exit(-1);
+	}
+	else if (result == 0) {
+		a = a+1;
+		procid = getpid(); // получение ид. текущего процесса
+		parentid = getppid(); // получение ид. род-го процесса
+		printf("Ид.процесса: %d, ид.родителя: %d, значение переменной a: %d\n ", procid, parentid, a);
+		result1 = execle("/bin/cat", "/bin/cat", "otchet_16.04.2024.txt", 0, envp);
+		printf("ошибка при выполнении системного вызова execle()\n");
+	        exit(-1);
+
+	}
+	else {
+		a = a + 555;
+		procid = getpid(); // получение ид. текущего процесса
+                parentid = getppid(); // получение ид. род-го процесса
+                printf("Ид.процесса: %d, ид.родителя: %d, значение переменной a: %d\n ", procid, parentid, a);
+	}
 	return 0;//Никогда не выполняется
 }
