d8ee93eb529963b0ed67781468a4aa06cd4c6967
A.A.Popova <bananabomm2003@mail.ru>
2024-04-30 13:05:59 +0700

    Добавили передачу информации от процесса родителя к ребенку

--- pipe.c
+++ pipe.c
@@ -5,6 +5,7 @@
 int main() {
 // Создаем массив из двух элементов для хранения файловых дескрипторов  для чтения и записи
         int fd[2];
+	pid_t result;
         size_t size;
         char string[] = "Hello, world!";
         char resstring[14];
@@ -13,26 +14,47 @@ int main() {
 // Возврат признака аврийного завершения программы (-1)
 		exit(-1);
 	}
-	size = write(fd[1], string, 14);
-// Проверка колличества байт, записанных в строку 
-	if(size != 14){
-		printf("Can\'t write all string\n");
+	result = fork();
+	if (result<0) {
+		printf("ошибка выполнения fork()\n");
 		exit(-1);
 	}
-	size = read(fd[0], resstring, 14);
-// Проверка колличества байт, прочитанных  из pipe 
-        if(size < 0){
-                printf("Can\'t read string\n");
-                exit(-1);
+	else if (result>0) {
+		if (close(fd[0])<0) {
+			printf("ошибка при закрытии pipe на чтение\n");
+			exit(-1);
+		}
+		size = write(fd[1], string, 14);
+		// Проверка колличества байт, записанных в строку 
+		if (size != 14) {
+			printf("Can\'t write all string\n");
+			exit(-1);
+		}
+		printf("процесс родитель записал информацию в pipe и заканчивает работу \n");
+		if (close(fd[1])<0) {
+                        printf("Can\'t close input stream\n");
+                        exit(-1);
+		}
+
 	}
-	printf("%s\n",resstring);
-//Закрытие pipe для чтения 
-	if(close(fd[0])<0){
-		printf("Can\'t close input stream\n");
-	}
-//Закрытие pipe для записи
-	if(close(fd[1])<0){
-		printf("Can\'t close output stream\n");
+	else {
+		if(close(fd[1])<0){
+        	        printf("Can\'t close input stream\n");
+			exit(-1);
+	        }
+	
+		size = read(fd[0], resstring, 14);
+		//Закрытие pipe для записи 
+        	if(size !=14){
+                	printf("Can\'t read string\n");		
+      		       	 exit(-1);	
+		}
+		printf("%s\n",resstring);
+		//Проверка колличества байт, прочитанных из pipe 
+		if(close(fd[0])<0){
+			printf("Can\'t close input stream\n");
+		exit(-1);
+		}
 	}
         return 0;
 }
