9e33755d9071d04d6749fe5fe3e45a21d7ee1785
A.A.Popova <bananabomm2003@mail.ru>
2024-06-04 11:37:17 +0700

    Использовали переменную экземпляра length

--- Array.java
+++ Array.java
@@ -8,19 +8,19 @@ class Array {
 		for(i = 0; i < 10; i++)
 			System.out.println("Элемент [" + i + "]: " + nums[i]);
 		//Адьтернативнй способ инициализации массива
-		int nums2[] = {99, 34, -7, 233, 76, -54, -76, 267, 987, 65};
+		int nums2[] = {99, 34, -7, 233, 76, -54, -76, 267, 987, 65, 45, 89, -23, 67};
 
 		int min, max;
 		//Поиск максимального и минимального значения
 		min = max = nums2[0];
-		for(i = 1; i < 10; i++) {
+		for(i = 1; i < nums2.length; i++) {
 			if(nums2[i] < min) min = nums2[i];
 			if(nums2[i] > max) max = nums2[i];
 		}
 		System.out.println("Минимальеное значение в массиве: " + min + "\nМаксимальное значение в массиве: " + max);
 		//Пузырьковая сортировка массива nums2
 		int a, b, t;
-		int size = 10;
+		int size = nums2.length;
 		System.out.println("Массив до сортировки:");
 		for(i = 0; i < size; i++)
 			System.out.println(" " + nums2[i]);
@@ -38,5 +38,10 @@ class Array {
                 for(i = 0; i < size; i++)
                         System.out.println(" " + nums2[i]);
 
+		// Использование переменной экземпляра length
+		System.out.println("Размер nums: " + nums.length);
+		System.out.println("Размер nums2: " + nums2.length);
+		for(i = 0; i < nums2.length; i++)
+			System.out.println(nums2[i]);
 	}
 }
